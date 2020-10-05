99d1314a9ae447b6377abf28146a4020679ac9d4
Sagar Ghodake <sagarghodake1432@gmail.com>
2020-10-05 23:09:49 +0530

    task completed

---
+++ src/pages/SignInForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SignInForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('The form was submitted with the following data:');
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+        <div className="FormCenter">
+            <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+            <div className="FormField">
+                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+              </div>
+
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="password">Password</label>
+                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+              </div>
+
+              <div className="FormField">
+              <label className="FormField__Label" htmlFor="role">Role</label><br/>
+              <input type="radio" id="male"   name="role"  value={this.state.male} onChange={this.handleChange}/>  Doctor <br/><br/>
+              <input type="radio" id="female"  name="role" value={this.state.female} onChange={this.handleChange} />  Admin
+              </div>
+            
+
+              <div className="FormField">
+                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
+              </div>
+            </form>
+          </div>
+        );
+    }
+}
+
+export default SignInForm;
